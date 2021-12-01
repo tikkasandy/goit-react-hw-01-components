32db659868f0c7870420d922e777fd4552616507
tikkasandy <82177700+tikkasandy@users.noreply.github.com>
2021-12-02 00:00:36 +0200

    Add ternary operator

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -2,16 +2,9 @@ import PropTypes from 'prop-types';
 import s from './FriendListItem.module.css';
 
 function FriendListItem({ avatar, name, isOnline }) {
-    let color;
-    if (isOnline) {
-        color = '#008000';
-    } else {
-        color = '#ff0000';
-    }
-
     return (
         <li className={s.item}>
-            <span className={s.status} style={{ backgroundColor: `${color}` }}></span>
+            <span className={s.status} style={isOnline ? { backgroundColor: 'green' } : { backgroundColor: 'red' }}></span>
             <img className={s.avatar} src={avatar} alt={name} width="48" />
             <p className={s.name}>{name}</p>
         </li >
